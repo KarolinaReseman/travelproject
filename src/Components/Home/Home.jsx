0a6014fdf8a0694949377c46acdb49fa7e9021a0
KarolinaReseman <reseman.karolina@gmail.com>
2023-09-10 19:43:04 +0200

    Added styles

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import "./Home.css"
 import Video from "../../Assets/video.mp4"
+import {AiOutlineSwapRight} from "react-icons/ai"
 
 const Home = () => {
   return (
@@ -12,10 +13,21 @@ const Home = () => {
       <div className='sectionText'>
         <h1>Unlock Your Travel Dreams With Us!</h1>
         <p>
-          
+          Discover the world's most adventurous nature, life is so short for a trip.
         </p>
+        <button className='btn flex'>GET STARTED <AiOutlineSwapRight className="icon"/></button>
       </div>
-
+      <div className="popularPlaces">
+        <div className="content">
+          <h3>Popular Places</h3>
+          <div className="images flex">
+            {/* <img src="" alt="" /> */}
+            {/* <img src="" alt="" /> */}
+            {/* <img src="" alt="" /> */}
+            {/* <img src="" alt="" /> */}
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
