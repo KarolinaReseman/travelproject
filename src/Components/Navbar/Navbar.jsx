f65d5d0066d3c49c62b0bca5f26dc917f5094927
KarolinaReseman <reseman.karolina@gmail.com>
2023-09-10 17:40:04 +0200

    add styles: Navbar.css ; add function showNavbar and removeNavBar

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,16 +1,26 @@
-import React from 'react'
+import React, {useState} from 'react'
+import "./Navbar.css"
 import {BiLogoMediumOld} from "react-icons/bi"
 import {AiFillCloseCircle} from "react-icons/ai"
 import {PiDotsNineBold} from "react-icons/pi"
 
 const Navbar = () => {
+  const [navBar, setNavBar] = useState("menu");
+  const showNavBar = () => {
+    setNavBar("menu showNavbar");
+  }
+
+  const removeNavBar =() => {
+    setNavBar("menu showNavBar");
+  }
+
   return (
     <div className='navBar'>
       <div className="logoDiv">
       <BiLogoMediumOld className="icon"/>
       <span>OU-Trips</span>
       </div>
-      <div className='menu'>
+      <div className={navBar}>
         <ul>
           <li className='navList'>
             Destination
@@ -25,12 +35,12 @@ const Navbar = () => {
             Gallery
           </li>
         </ul>
-        <AiFillCloseCircle className="icon"/>
+        <AiFillCloseCircle className="icon closeIcon" onClick={removeNavBar}/>
       </div>
       <button className="signUpBtn btn">
         Sign Up 
       </button>
-      <PiDotsNineBold className="icon"/>
+      <PiDotsNineBold className="icon menuIcon" onClick={showNavBar}/>
     </div>
   )
 }
