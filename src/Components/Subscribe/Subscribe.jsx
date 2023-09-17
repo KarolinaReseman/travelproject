d2bc16bb933b439f75b2fa5fb4db024fa4ef30e6
KarolinaReseman <reseman.karolina@gmail.com>
2023-09-17 14:18:30 +0200

    added image and styles

--- src/Components/Subscribe/Subscribe.jsx
+++ src/Components/Subscribe/Subscribe.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
 import "./Subscribe.css"
+import image from "../../Assets/women.jpg"
 
 const Subscribe = () => {
   return (
-    <div>
+    <div className='subscribe section container'>
+      <div className="sec Container grid">
+        <img src={image} alt='Div Image'/>
+
+        <div className='textDiv'>
+          <h4>Best Way To Start Your Journey!</h4>
+          <p>
+            We offer personalized itineraries tailored to individual preferences and interests.
+          </p>
+          <button className='btn'> Start Here</button>
+        </div>
+      </div>
     </div>
   )
 }
